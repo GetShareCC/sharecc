bc9d15a08cad59e37389da28774e0b988a35c5f8
SomeRandom-Dev <46102926+SomeRandom-Dev@users.noreply.github.com>
2021-02-20 09:20:46 +0000

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
-echo "Welcome To Scribe Builder"
-echo "Building 'main.cpp' with arguments -lsfml-graphics -lsfml-window -lsfml-system"
-g++ installer.cpp -o scribe -lsfml-graphics -lsfml-window -lsfml-system
+echo "This sh script will build ShareCC"
+echo "Building 'installer.cpp' with arguments -lsfml-graphics -lsfml-window -lsfml-system"
+g++ installer.cpp -o ShareCC -lsfml-graphics -lsfml-window -lsfml-system
 echo "Done compiling! Now running."
-./scribe
+./ShareCC
 echo "Scribe has terminated! If you closed the program yourself, this is fine. But if this happened shortly after the last message, there may be an error in your code."
